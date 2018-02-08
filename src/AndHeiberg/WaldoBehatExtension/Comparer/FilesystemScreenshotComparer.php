c9a2f500899f5987208b86b22778c513c421181d
Andreas Heiberg <andreas@heiberg.io>
2018-02-08 13:13:15 +0000

    feat: colocate diff and screenshot
    Having diffs in an outputs folder got annoying.
    
    Signed-off-by: Andreas Heiberg <andreas@heiberg.io>

--- src/AndHeiberg/WaldoBehatExtension/Comparer/FilesystemScreenshotComparer.php
+++ src/AndHeiberg/WaldoBehatExtension/Comparer/FilesystemScreenshotComparer.php
@@ -3,9 +3,10 @@
 namespace AndHeiberg\WaldoBehatExtension\Comparer;
 
 use Intervention\Image\ImageManager;
+use League\Flysystem\File;
 use League\Flysystem\FilesystemInterface;
 
-    class FilesystemScreenshotComparer implements ScreenshotComparerInterface
+class FilesystemScreenshotComparer implements ScreenshotComparerInterface
 {
     /**
      * @var FilesystemInterface
@@ -27,22 +28,28 @@ use League\Flysystem\FilesystemInterface;
      */
     public function compare($screenshot)
     {
-        $position = strpos($screenshot, '/');
+        $pathMinusTimestamp = substr($screenshot, strpos($screenshot, '/') + 1);
 
-        $base = '/base/'.substr($screenshot, $position + 1);
-        $output      = '/output/'.substr($screenshot, $position + 1);
+        $extensionPosition = strpos($screenshot, '.');
+        $extension = substr($screenshot, $extensionPosition);
+        $pathMinusExtension = substr($screenshot, 0, $extensionPosition);
 
-        $base = $this->filesystem->get($base);
-        $screenshot = $this->filesystem->get($screenshot);
+        $basePath = '/base/'.$pathMinusTimestamp;
+        $outputPath = "{$pathMinusExtension}_diff{$extension}";
+
+        /** @var File $baseFile */
+        $baseFile = $this->filesystem->get($basePath);
+        /** @var File $screenshotFile */
+        $screenshotFile = $this->filesystem->get($screenshot);
 
         $manager = new ImageManager(['driver' => 'imagick']);
 
-        $screenshot = $manager->make($screenshot->readStream());
-        $base = $manager->make($base->readStream());
+        $screenshotImage = $manager->make($screenshotFile->readStream());
+        $baseImage = $manager->make($baseFile->readStream());
 
-        $compare = $base->compare($screenshot);
+        $compare = $baseImage->compare($screenshotImage);
 
-        $this->filesystem->put($output, $compare->getDiffImage()->encode('png'));
+        $this->filesystem->put($outputPath, $compare->getDiffImage()->encode('png'));
 
         return new ComparisonResult($compare->getScore(), $compare->getDiffImage());
     }
